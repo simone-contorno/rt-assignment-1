3ef51600edbd3ab08447e0cc4231e3b2e50bf55b
Simone Contorno <83269499+simone-contorno@users.noreply.github.com>
2021-11-06 11:51:27 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -24,36 +24,32 @@ How this program could be improved.<br>
 <a name="intro"></a>
 ### Introduction
 
-rt_assigment_1 uses the library sr.robot to obtain information about the robot and to control it.<br>
+rt_assigment_1 uses the library sr.robot to get information about the robot and to control it.<br>
 In particular, the robot:
     <ol>
         <li>Follows a path.</li>
         <li>Avoids the golden tokens which rappresent the borders.</li>
-        <li>Grabs a silver token when it is near to them and move it behind itself.</li>
+        <li>Grabs a silver token when it is near to it and move it behind itself.</li>
     </ol>
 <br>
-At the start it looks like this:
+At the begin it looks like:
 
 ![map](https://github.com/simone-contorno/rt_assignment_1/blob/main/map.png)
 
 <a name="how"></a>
 ### How it works
 
-The program use principally 2 parameters:
-    <ol>
-        <li>Direction: signal the direction where the robot is going on.</li>
-            <ul>
-                <li>1 - Right</li>
-                <li>2 - Up</li>
-                <li>3 - Left</li>
-                <li>4 - Right</li>
-            </ul>
-        <li>Memory: memorize the last direction (with the opposite direction value).</li>
-    </ol>
-Using these variables the robot can follow the path without worrying about the next silver token and adjusting the direction to remain almost parallel to the borders.<br>
-When it is too close to a golden token (in front of it), the robot turns, searching a new free direction.<br>
-When it is close enough to a silver token, the robot starts to align itself with the token and reaches it to grab and move it behind itself.<br>
-Look the pseudo-code file for more details.<br>
+Let's analyze each case:
+    <ul>
+        <li>If there is an angle too small between the robot and a golden token, the robot turns away to be almost parallel to the borders.</li>
+        <li>If there is a distance too small between the robot and a silver token, the robot starts a check to identify where the path is free;
+            to do this, it checks the distance from the wall on the right and from the wall on the left (walls are represented by golden token)
+            and go to the furthest one.<li>
+        <li>If there is not any golden token between the robot and a silver token and the distance between them is little enough (less than the
+            distance between the robot and the closest golden token), the robot turn against the silver token and go on.</li>
+        <li>When the robot is close enough to the silver token, it grabs this one, move it behind itself and turn again to continue the path.</li>
+    </ul>
+Look the pseudocode file for more details.<br>
 
 <a name="installation"></a>
 ### Installation and Execution
@@ -66,7 +62,7 @@ First of all install the simulated environment from github opening the terminal 
     </code>
 </pre>
 
-After, go in the folder 'python_simulator' and afterwards into 'robot-sim', where there is the file 'run.py',
+After, go in the folder 'python_simulator', afterwards into 'robot-sim', where there is the file 'run.py',
 and download rt_assignment_1 typing:
 
 <pre>
@@ -83,13 +79,19 @@ Now, to run 'assignment.py' type:
     </code>
 </pre>
 
-You can also change some parameters into the code to modify the robot performance (e.g. straight_on_speed, turn_speed and refresh_rate).<br>
+You can also change some parameters into the code to modify the robot performance (e.g. straight_on_speed and refresh_rate).<br>
 You will find all the information about functions and variables in the file thanks to the comments.
 
 <a name="improve"></a>
 ### Improvements
 
-The robot view could be improved to a better performance: <br>
-instead of turn when it is against a wall, it can turns following better rules, like checking the distance of more than one golden token and computing the path direction to follow it.<br>
+Some possible and greatful improvements: <br>
+    <ul>
+        <li>The robot could turn better with a better check about the golden tokens around it.</li>
+        <li>The robot could identify better the silver token, for instance it could go to reach it
+            only when there is not any golden token bewteen them, without the second check about the
+            distance between the robot and the closest golden token.<li>
+    </ul>
+Thanks to have read this file, i hope it was been clear and interesting.<br>
 <br>
 Bye.    :slightly_smiling_face:
--- assignment.py
+++ assignment.py
@@ -18,8 +18,7 @@ a_th = 2 # Threshold for the control of the orientation
 d_th = 0.4 # Threshold for the control of the linear distance
 
 # max_speed = 100
-straight_on_speed = 75 # Straight on speed
-turn_speed = 12.5 # Turn speed
+straight_on_speed = 100 # Straight on speed
 kp = 1 # Gain
 margin_error = 1 # Spin precision
 
@@ -30,38 +29,16 @@ Parameters from golden tokens:
     2. min_angle_from_g : if the robot has an angle less than this one and the distance
     from the golden token is less than dist_from_g, it turns of turn_from_g
 '''
-max_angle_from_g = 82.5 
-min_angle_from_g = 10            
+max_angle_from_g = 80
+min_angle_from_g = 10       
 dist_from_g = 1
 turn_from_g = 90
 
-'''
-Refresh rate: decrement this value to obtain a more precision with the robot's movements
-'''
-refresh_rate = 0.05 
+refresh_rate = 0.05 # Decrement this value to obtain more precision with the robot's movements
 
-'''
-Memory system variables:
-    - direction : signal the direction that the robot is going on:
-        1 - Right
-        2 - Up
-        3 - Left
-        4 - Down
-    - memory : memorize the last direction (with the opposite value).
-    - turn_value : the robot turns of this value if direction is equals to the memory
-    - distance : update memory value only when the robot ran this value
-'''
-direction = 4 # Initial direction
-memory = 2 # Initial memory
-turn_value = 90 
-distance = 0 # Initial distance
-distance_to_run = 0.06 
-
-old_direction = 0 # Just to manage how often print direction
 print_flag = 0 # Just to manage printed strings
 
-# Instance of the class Robot
-R = Robot()
+R = Robot() # Instance of the class Robot
 
 '''
     ---------- FUNCTIONS ----------
@@ -81,13 +58,13 @@ def drive(speed, seconds):
     R.motors[0].m0.power = 0
     R.motors[0].m1.power = 0
 
-"""
+'''
 Function to turn
     
 Args: 
     speed (int): the speed of the wheels
 	seconds (int): the time interval
-"""
+'''
 def turn(speed, seconds):
     R.motors[0].m0.power = speed
     R.motors[0].m1.power = -speed
@@ -132,42 +109,12 @@ def find_token_gold():
    	    return dist, rot_y
 
 '''
-Function to move behind the silver token when the robot grabs it
-'''
-def move_behind() :
-    turn_P(180)
-    R.release()
-    turn_P(180)
-
-'''
-Function to check orientation and update direct values 
-'''
-def update(direction) :
-
-    # Update direction 
-    heading = R.heading * 180 / math.pi
-
-    if -45 <= heading < 45 :
-        direction = 1
-    elif -135 <= heading < -45 :
-        direction = 2
-    elif -180 <= heading < -135 :
-        direction = 3
-    elif 45 <= heading < 135 :
-        direction = 4
-    elif 135 <= heading <= 180 :
-        direction = 3
-
-    return direction
-
-'''
-Function to turn the robot of a grades value :
+Function to turn the robot of grades (arg) value :
     1. Take current orientation
     2. Compute final orientation
     3. Turn until the orientation is equals to the computed final orientation
 '''
 def turn_P(grades) :
-
     # 1. Take current orientation
     heading = R.heading * 180 / math.pi
     if -180 <= heading < 0 :
@@ -176,7 +123,7 @@ def turn_P(grades) :
         heading = 360 - heading
     
     # 2. Compute final orientation
-    final = heading + grades
+    final = heading + module(grades)
 
     final_flag = 0
 
@@ -190,8 +137,11 @@ def turn_P(grades) :
     
     # 3. Turn until the error is more than max error set
     while error > margin_error :
-        turn(-error * kp, refresh_rate)
-
+        if grades > 0 :
+            turn(-error * kp, refresh_rate)
+        else : 
+            turn(+error * kp, refresh_rate)
+        
         heading = R.heading * 180 / math.pi
         if -180 <= heading < 0 :
             heading = module(heading)
@@ -203,9 +153,39 @@ def turn_P(grades) :
         else :
             error = final - heading
             error = module(error)
+        
+        if error > 180 :
+            error = 360 - error
 
 '''
-Function to return the module value
+Function to move behind the silver token when the robot grabs it:
+    the robot checks if there is more space on the right or on the left 
+    to avoid the possibility of hurting the silver token against a golden 
+    token during the spin.
+'''
+def move_behind() :
+    # Check token on the right
+    dist_1 = 100
+    for token in R.see(): 
+        if token.info.marker_type is MARKER_TOKEN_GOLD and token.dist < dist_1 and 80 < token.rot_y < 100 :
+            dist_1 = token.dist
+        
+    # Check token on the left
+    dist_2 = 100
+    for token in R.see(): 
+        if token.info.marker_type is MARKER_TOKEN_GOLD and token.dist < dist_2 and -100 < token.rot_y < -80 :
+            dist_2 = token.dist
+    
+    if dist_1 > dist_2 : # Turn right
+        turn_P(-180)
+    else : # Turn left
+        turn_P(+180)
+
+    R.release()
+    turn_P(180)
+
+'''
+Function to get the module of rot (arg)
 '''
 def module(rot) :
     if rot < 0 :
@@ -222,9 +202,6 @@ time.sleep(3)
 print("GO!\n")
 
 while 1:
-    # Update direction value
-    direction = update(direction)
-
     # Search the closest silver token
     dist_s, rot_s = find_token_silver()
 
@@ -241,104 +218,97 @@ while 1:
     if dist_s == -1:
         print("I don't see any silver token... :(")
     else :
-        # Check if it is close enough to grab
+        # Check if the robot is close enough to grab the silver token
         if dist_s < d_th and rot_s_m < a_th : 
-            R.grab() # If the robot is close to the token, it grabs it
+            R.grab() 
             print("Gotcha!\n") 
-            move_behind() # Move the block behind it
+            move_behind() # Move the token behind 
+            print_flag = 1
 
     '''
     Check if the robot has not any golden token between it and the silver token; 
     in this case it turns against the silver token iff the angle with this one
     is not too much, otherwise it go on to reach it.
     '''
-    if (dist_s < dist_g and rot_s_m < 90) or 0 <= rot_s_m < 10 and direction != memory :
-        
-        # Aligned with the silver token
+    if (dist_s < dist_g and rot_s_m < 90) :
+        if print_flag == 0 : 
+            print("Silver token found!")
+            print_flag = 2
+        elif print_flag == 1 :
+            print_flag = 0
+
+        # Go on
         if rot_s_m < a_th : 
-            drive(+straight_on_speed, refresh_rate)
+            drive(+dist_s * straight_on_speed, refresh_rate)
      
         # Align with the silver token
         else : 
             if rot_s < 0 : # Turn left
-                turn(-turn_speed, refresh_rate)
-
+                turn(-rot_s_m, refresh_rate)
             else : # Turn right
-                turn(+turn_speed, refresh_rate)
-                
-            NULL, rot_s = find_token_silver()
-            rot_s_m = module(rot_s)
+                turn(+rot_s_m, refresh_rate)
 
         continue
-    
+  
     '''
     Check if the robot is oriented too close to a golden token (the angle between them is too small)
     '''
-    if min_angle_from_g <= rot_g_m < max_angle_from_g :
+    if min_angle_from_g <= rot_g_m < max_angle_from_g and dist_g < 1 :
+        error = 90 - rot_g_m
 
         if rot_g < 0 : # Turn right
-            turn(+turn_speed, refresh_rate)   
-
+            turn(+error, refresh_rate)   
         else : # Turn left
-            turn(-turn_speed, refresh_rate) 
+            turn(-error, refresh_rate) 
 
         continue
 
     '''
-    Check if the robot is too close to a golden token (almost in front of it)
-    '''
-    if dist_g < dist_from_g and rot_g_m < min_angle_from_g :
-        turn_P(turn_from_g)
-        distance = 0
-
-    '''
-    Check 
-    
-    If the direction is not equals to the memory:
-        1. The robot go 
-        2. The memory value will be update after a distance value.
-        3. Print new values.
-    
-    If the direction is equals to the memory the robot turns left of 90 grades.
-    '''
-    if direction != memory : 
-
-        # 1. Compute the distance
-        temp_dist_g, NULL = find_token_gold()
-        drive(straight_on_speed, refresh_rate)
-        dist_g, NULL = find_token_gold()
-
-        # The difference must be small (otherwise it means that the robot changed the reference token)
-        if dist_g > temp_dist_g and dist_g - temp_dist_g < 0.05 :
-            distance += (dist_g - temp_dist_g)
-
-        elif dist_g <= temp_dist_g and temp_dist_g - dist_g < 0.05 : 
-            distance += (temp_dist_g - dist_g)
-            
-        # 2. Update the memory
-        if distance >= distance_to_run : 
-            distance = 0
-
-            if direction == 1 :
-                memory = 3
-            elif direction == 2 :
-                memory = 4
-            elif direction == 3 :
-                memory = 1
-            else :
-                memory = 2
-
-            if print_flag == 1 :
-                print("New memory: " + str(memory))
-                print_flag = 0
+    Check if the robot is too close to a golden token (almost in front of it) and turn it towards the free direction
+    '''     
+    # Check the golden token in front of the robot
+    dist_0 = 100
+    for token in R.see() : 
+        if token.info.marker_type is MARKER_TOKEN_GOLD and token.dist < dist_0 and -min_angle_from_g < token.rot_y < min_angle_from_g :
+            dist_0 = token.dist
         
-        # 3. Print direction value
-        if old_direction != direction :
-            print("New direction = " + str(direction))
-            old_direction = direction
-            distance = 0
-            print_flag = 1
+    if dist_0 < 1 : # Change this value depending the width of the path 
+        # Check token on the right
+        dist_1 = 100
+        for token in R.see() : 
+            if token.info.marker_type is MARKER_TOKEN_GOLD and token.dist < dist_1 and 80 < token.rot_y < 100 :
+                dist_1 = token.dist
+        
+        if dist_1 == 100 :
+            print("Error! I did not find any golden token on the right...")
 
-    else : # direction == memory
-        turn_P(turn_value) 
-        distance = 0
+        # Check token on the left
+        dist_2 = 100
+        for token in R.see() : 
+            if token.info.marker_type is MARKER_TOKEN_GOLD and token.dist < dist_2 and -100 < token.rot_y < -80 :
+                dist_2 = token.dist
+
+        if dist_2 == 100 :
+            print("Error! I did not find any golden token on the left...")
+
+        # print("Right distance: " + str(dist_1))
+        # print("Left distance: " + str(dist_2) + "\n")
+        if dist_1 > dist_2 : # Turn right
+            print("Turn right...")
+        else : # Turn left
+            print("Turn left...")
+
+        while dist_0 < 3 : # Change this value depending the width of the path
+            if dist_1 > dist_2 : # Turn right
+                turn(+straight_on_speed / dist_0, refresh_rate)
+            else : # Turn left
+                turn(-straight_on_speed / dist_0, refresh_rate)
+            
+            NULL, rot_g = find_token_gold()
+            rot_g_m = module(rot_g)
+
+            for token in R.see() : 
+                if token.info.marker_type is MARKER_TOKEN_GOLD and token.dist > dist_0 and -min_angle_from_g < token.rot_y < min_angle_from_g :
+                    dist_0 = token.dist
+
+    drive(+straight_on_speed, refresh_rate)
